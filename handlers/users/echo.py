091121e1aa98b1d41b7fa35ddf04c75838b5f026
qustoo <58177905+qustoo@users.noreply.github.com>
2021-03-29 21:01:05 +0300

    изменение в эхо

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -6,14 +6,12 @@ from loader import dp
 # Сюда летят только текстовые сообщения
 @dp.message_handler(content_types=types.ContentType.TEXT)
 async def bot_echo(message: types.Message):
-    # Получил чат айди и текст сообщения
-    chat_id = message.chat.id
-    text = message.text
 
     # Импортируем бота
     from loader import bot
 
-    await bot.send_message(chat_id=chat_id, text=text)
+    # Получил чат айди и текст сообщения
+    await bot.send_message(chat_id=message.chat.id, text=message.text)
 
     # Второй вариант без chat_id
     # await message.answer(text=text)
