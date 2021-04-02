6a2260b956a968541941d05d351b0f9082826b2f
qustoo <58177905+qustoo@users.noreply.github.com>
2021-04-03 00:55:28 +0300

    Пробные кнопки под тесты

--- handlers/users/quizing_easy.py
+++ handlers/users/quizing_easy.py
@@ -4,6 +4,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 
 from data.question_quizing_easy import Easy_Array_Questions
+from keyboards.default import menu
 from loader import dp
 from aiogram import types
 
@@ -15,15 +16,16 @@ async def enter_easy_test(message: types.Message, state: FSMContext):
     rand = random.randint(0, (len(Easy_Array_Questions) - 1))
     if rand == 0:
         # вместо Node - передаем клаву для ответа, и так для каждого вопроса
-        await message.answer(text=f"Ответ на вопрос {rand}", reply_markup=None)
+        await message.answer(text=f"Ответ на вопрос {rand}", reply_markup=menu.menu1.keyboard)
     elif rand == 1:
-        await message.answer(text=f"Ответ на вопрос {rand}")
+        await message.answer(text=f"Ответ на вопрос {rand}", reply_markup=menu.menu2.keyboard)
+        '''
     elif rand == 2:
         await message.answer(text=f"Ответ на вопрос {rand}")
     elif rand == 3:
         await message.answer(text=f"Ответ на вопрос {rand}")
 
-        '''
+        
         await message.answer(text=
                              Easy_Array_Questions[
                                  random.randint(0, (len(Easy_Array_Questions) - 1))])
@@ -31,3 +33,4 @@ async def enter_easy_test(message: types.Message, state: FSMContext):
         # QuizEasy.Q1.set()
         '''
         await state.finish()
+
--- keyboards/default/menu.py
+++ keyboards/default/menu.py
@@ -1,16 +1,35 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-menu = ReplyKeyboardMarkup(
+menu1 = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text="Церковь Святого Ивана")
+            KeyboardButton(text="Ответ №1")
         ],
         [
-            KeyboardButton(text="Памятник Раку")
+            KeyboardButton(text="Ответ №2")
 
         ],
         [
-            KeyboardButton(text="Аллея Роз")
+            KeyboardButton(text="Ответ №3")
+        ],
+
+    ],
+    # чтобы размер был нормальным, клава не занимала пол-экрана
+    resize_keyboard=True
+
+)
+
+menu2 = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Ответ №11")
+        ],
+        [
+            KeyboardButton(text="Ответ №22")
+
+        ],
+        [
+            KeyboardButton(text="Ответ №33")
         ],
 
     ],
