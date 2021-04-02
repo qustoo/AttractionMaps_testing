4c4aa25b03e56f02d71c7faf8726f89d9d9fd094
dimka <sawer_13@mail.ru>
2021-04-02 18:43:33 +0300

    пробное написание 3

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -4,12 +4,12 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp, bot
 from utils.misc import rate_limit
 
-#12345
-#5678
+#2.04 опробовали git
+#abra
 # лимит на раз в 5 сек
 @rate_limit(limit=5)
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f'Привет,{message.from_user.full_name}! 😎'
+    await message.answer(f'Привет!!!,{message.from_user.full_name}! 😎'
                          f'\nРады тебя видеть!'
                          f'\nДля получения списка комманд нажми /help')
