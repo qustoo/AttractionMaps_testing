a91302d1f60ee7f4c6850c6d008c621422df3384
qustoo <58177905+qustoo@users.noreply.github.com>
2021-04-02 18:49:15 +0300

    Принимаю изменения от Димы 02.04 18:49

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -4,8 +4,13 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp, bot
 from utils.misc import rate_limit
 
-#2.04 опробовали git
-#abra
+
+# 2.04 опробовали git
+# abra
+# принял изменения
+
+
+
 # лимит на раз в 5 сек
 @rate_limit(limit=5)
 @dp.message_handler(CommandStart())
