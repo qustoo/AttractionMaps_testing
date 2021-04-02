2c576c987e1df3970573abadfc986ba76256b185
qustoo <58177905+qustoo@users.noreply.github.com>
2021-04-02 22:45:18 +0300

    Откат старта

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -14,6 +14,5 @@ from utils.misc import rate_limit
 @rate_limit(limit=5)
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f'Здра!!!!вствуй, {message.from_user.full_name}! 😎😉'
-                         f'\nДля получения спис!!!ка д!!!оступных коман!!д нажми /help'
-                         f'dsgyhmwsdiopgnsdoigsd klgnsdopjigsdnpiou')
+    await message.answer(f'Здравствуйте, {message.from_user.full_name}! 😎😉'
+                         f'\nДля получения списка доступных команд нажми /help')
