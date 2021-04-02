656763f517e76bf05b128ca4888e1641dee5113a
qustoo <58177905+qustoo@users.noreply.github.com>
2021-04-02 19:22:51 +0300

    Добавил смайликов в старт

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -10,11 +10,10 @@ from utils.misc import rate_limit
 # принял изменения
 
 
-
 # лимит на раз в 5 сек
 @rate_limit(limit=5)
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f'Привет!!!,{message.from_user.full_name}! 😎'
+    await message.answer(f'Привет!!!,{message.from_user.full_name}! 😎😎😎'
                          f'\nРады тебя видеть!'
                          f'\nДля получения списка комманд нажми /help')
