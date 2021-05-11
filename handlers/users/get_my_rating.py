bec263a322d006b1a44573216701b2db71875b29
alexred <alexsandr.2002.m@gmail.com>
2021-05-11 20:17:42 +0300

    Реализация комманд /get_my_rating и /get_rating

--- handlers/users/get_my_rating.py
+++ handlers/users/get_my_rating.py
@@ -4,32 +4,26 @@ from aiogram.dispatcher.filters import Command
 from loader import dp, db
 
 
+@dp.message_handler(Command("get_my_rating"))
+async def get_my_rating(message: types.Message):
+    rate = db.select_user(id=message.from_user.id)[-1]
+    await message.answer("Ваш рейтинг: " + str(rate))
+
+
 @dp.message_handler(Command("get_rating"))
 async def view_rating_user(message: types.Message):
-    # внутри бд такой порядок переменных
-    # (id,name,email,lat,lon,rating)
-    await message.answer(f'Ваш рейтинг в системе викторины = {db.select_user(id=message.from_user.id)[-1]}')
-    list_of_users = [(434354, 'Jaroslaw', 3.0),
-                     (245636, 'Daria', 8.0),
-                     (234236, 'Vika', 5.0),
-                     (326267, 'Nastya', 9.0)]
-    # выборка всех пользователей из бд db.select_all_users()
-    # словарь для хранения переменных в виде: Имя - Рейтинг
-    temp_dict_users = {}
+    users = db.select_all_users()
+    print(len(users))
+    tmp_user_rating = {}
+    for i in users:
+        tmp_user_rating[i[1]] = i[-1]
+    sorted_user_rating = {}
+    sorted_users = sorted(tmp_user_rating, key=tmp_user_rating.get, reverse=True)
+    for w in sorted_users:
+        sorted_user_rating[w] = tmp_user_rating[w]
 
-    # заполняем словарь именами и рейтингом
-    for i in list_of_users:
-        temp_dict_users[i[1]] = i[-1]
+    output_string = ""
+    for user in sorted_user_rating:
+        output_string += "Рейтинг пользователя " + user + ": " + str(sorted_user_rating[user])
+    await message.answer(output_string)
 
-    # формируем на лету новый словарь, сортируем по убыванию, от наибольшего к наименьшему рейтингу
-    dict_rating_users = {k: temp_dict_users[k] for k in sorted(temp_dict_users, key=temp_dict_users.get, reverse=True)}
-
-    answer_str = ""
-    index = 1
-    # формируем готовую строчку c местом, именем и рейтингом
-    for user in dict_rating_users.items():
-        answer_str = answer_str + str(index) + ':' + ' ' + user[0] + ' - ' + str(user[1]) + '\n'
-        index += 1
-    # Отправлую лишь одну строчку пользователю чтобы не присылать 100 сообщений со звуком уведомления
-    await message.answer(text='Топ Рейтинга пользователей:\n'
-                              f'{answer_str}')
