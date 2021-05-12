af4b49aa422e99d614dd0874eb8ba2c892791232
qustoo <58177905+qustoo@users.noreply.github.com>
2021-05-12 22:37:05 +0300

    cache_time = 20 in kirosvky

--- handlers/users/encyclopedia_district/Kirovsky_district/encyclopedia_kirovsky.py
+++ handlers/users/encyclopedia_district/Kirovsky_district/encyclopedia_kirovsky.py
@@ -10,27 +10,28 @@ from keyboards.inline.encyclopedia import get_page_keyboard, pagination_call, ge
 from loader import dp, photo_db
 
 
-
 @dp.callback_query_handler(place_callback.filter(item_name="kir_r"))
 async def kir_region_pag(call: CallbackQuery, callback_data: dict):
-    await call.answer(cache_time=60)
-    text= get_page(Kirovsky_text)
-    photo=get_page(Kirovsky_photo)
-    await call.message.answer_photo(photo=open(photo_db.get_one_file_name(name=photo), 'rb'), reply_markup=get_page_keyboard(k="kir",max=max_kir_photo))
-    await call.message.answer(text=text, reply_markup=get_text(max=max_kir,k="kir_t"))
+    await call.answer(cache_time=20)
+    text = get_page(Kirovsky_text)
+    photo = get_page(Kirovsky_photo)
+    await call.message.answer_photo(photo=open(photo_db.get_one_file_name(name=photo), 'rb'),
+                                    reply_markup=get_page_keyboard(k="kir", max=max_kir_photo))
+    await call.message.answer(text=text, reply_markup=get_text(max=max_kir, k="kir_t"))
 
 
 @dp.callback_query_handler(pagination_call.filter(key="kir"))
 async def show_current_page(call: CallbackQuery, callback_data: dict):
-    current_page=int(callback_data.get("page"))
-    photo = get_page(array=Kirovsky_photo,page=current_page)
-    media=InputMediaPhoto(media=open(photo_db.get_one_file_name(name=photo),'rb'))
-    markup=get_page_keyboard(k="kir",max=max_kir_photo,page=current_page)
+    current_page = int(callback_data.get("page"))
+    photo = get_page(array=Kirovsky_photo, page=current_page)
+    media = InputMediaPhoto(media=open(photo_db.get_one_file_name(name=photo), 'rb'))
+    markup = get_page_keyboard(k="kir", max=max_kir_photo, page=current_page)
 
-    await call.message.edit_media(media= media, reply_markup=markup)
+    await call.message.edit_media(media=media, reply_markup=markup)
+
 
 @dp.callback_query_handler(pagination_call.filter(key="kir_t"))
 async def show_current_page(call: CallbackQuery, callback_data: dict):
-    current_page=int(callback_data.get("page"))
-    text=get_page(array=Kirovsky_text,page=current_page)
-    await call.message.edit_text(text=text, reply_markup=get_text(k="kir_t",max=max_kir,page=current_page))
+    current_page = int(callback_data.get("page"))
+    text = get_page(array=Kirovsky_text, page=current_page)
+    await call.message.edit_text(text=text, reply_markup=get_text(k="kir_t", max=max_kir, page=current_page))
