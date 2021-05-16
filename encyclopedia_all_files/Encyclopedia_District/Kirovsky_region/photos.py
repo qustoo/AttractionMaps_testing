3e33b5957f67eddec8f861ef479d1ae3445ab922
DaAria <dkl@sfedu.ru>
2021-05-16 20:37:00 +0300

    дополнены достопримечательности

--- encyclopedia_all_files/Encyclopedia_District/Kirovsky_region/photos.py
+++ encyclopedia_all_files/Encyclopedia_District/Kirovsky_region/photos.py
@@ -5,6 +5,6 @@ Kirovsky_photo=[
 
   "encyclopedia_kirovsky_guestion_13", "encyclopedia_kirovsky_guestion_14","encyclopedia_kirovsky_guestion_15","encyclopedia_kirovsky_guestion_16","encyclopedia_kirovsky_guestion_17","encyclopedia_kirovsky_guestion_18",
 
-    "encyclopedia_kirovsky_guestion_19","encyclopedia_kirovsky_guestion_20"
+    "encyclopedia_kirovsky_guestion_19","encyclopedia_kirovsky_guestion_20","encyclopedia_kirovsky_guestion_21"
 ]
 max_kir_photo=len(Kirovsky_photo)
--- encyclopedia_all_files/Encyclopedia_District/MayDay_region/photos.py
+++ encyclopedia_all_files/Encyclopedia_District/MayDay_region/photos.py
@@ -1,5 +1,7 @@
 MayDay_photo = [
     "encyclopedia_mayday_guestion_1", "encyclopedia_mayday_guestion_2", "encyclopedia_mayday_guestion_3","encyclopedia_mayday_guestion_4","encyclopedia_mayday_guestion_5",
     "encyclopedia_mayday_guestion_6","encyclopedia_mayday_guestion_7","encyclopedia_mayday_guestion_8","encyclopedia_mayday_guestion_9","encyclopedia_mayday_guestion_10",
+    "encyclopedia_mayday_guestion_11","encyclopedia_mayday_guestion_12","encyclopedia_mayday_guestion_13","encyclopedia_mayday_guestion_14","encyclopedia_mayday_guestion_15",
+    "encyclopedia_mayday_guestion_16","encyclopedia_mayday_guestion_17",
 ]
 
