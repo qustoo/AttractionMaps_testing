ecb9a98bda0113646b93ee043bb2c0293b1f0199
DaAria <dkl@sfedu.ru>
2021-05-16 23:05:11 +0300

    все достопримечательности

--- encyclopedia_all_files/Encyclopedia_District/Soviet_region/photos.py
+++ encyclopedia_all_files/Encyclopedia_District/Soviet_region/photos.py
@@ -1,4 +1,10 @@
-Sovet_photo= ["encyclopedia_guestion_1",'encyclopedia_guestion_2',"encyclopedia_guestion_3","encyclopedia_guestion_4","encyclopedia_guestion_5",
-               'encyclopedia_guestion_6',"encyclopedia_guestion_7","encyclopedia_guestion_8","encyclopedia_guestion_9","encyclopedia_guestion_10"
-]
-max_sov=len(Sovet_photo)
+Sovet_photo = ["encyclopedia_guestion_1", 'encyclopedia_guestion_2', "encyclopedia_guestion_3",
+               "encyclopedia_guestion_4", "encyclopedia_guestion_5",
+               'encyclopedia_guestion_6', "encyclopedia_guestion_7", "encyclopedia_guestion_8",
+               "encyclopedia_guestion_9", "encyclopedia_guestion_10",
+               "encyclopedia_guestion_11", "encyclopedia_guestion_12", "encyclopedia_guestion_13",
+               "encyclopedia_guestion_14", "encyclopedia_guestion_15",
+               "encyclopedia_guestion_16", "encyclopedia_guestion_17", "encyclopedia_guestion_18",
+               "encyclopedia_guestion_19",
+               ]
+max_sov = len(Sovet_photo)
--- encyclopedia_all_files/Encyclopedia_District/Voroshilovsky_region/photos.py
+++ encyclopedia_all_files/Encyclopedia_District/Voroshilovsky_region/photos.py
@@ -1,5 +1,7 @@
 Voroshil_photo=[
 "encyclopedia_voroshilovsky_guestion_1","encyclopedia_voroshilovsky_guestion_2","encyclopedia_voroshilovsky_guestion_3","encyclopedia_voroshilovsky_guestion_4","encyclopedia_voroshilovsky_guestion_5",
-    "encyclopedia_voroshilovsky_guestion_6",
+    "encyclopedia_voroshilovsky_guestion_6","encyclopedia_voroshilovsky_guestion_7","encyclopedia_voroshilovsky_guestion_8","encyclopedia_voroshilovsky_guestion_9","encyclopedia_voroshilovsky_guestion_10",
+"encyclopedia_voroshilovsky_guestion_11","encyclopedia_voroshilovsky_guestion_12","encyclopedia_voroshilovsky_guestion_13","encyclopedia_voroshilovsky_guestion_14","encyclopedia_voroshilovsky_guestion_15",
+"encyclopedia_voroshilovsky_guestion_16",
 ]
 max_vor=len(Voroshil_photo)
